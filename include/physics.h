28a2ce78886a2d493dc3e2dbb72e5cbd5ff8f73a
Niek <niekmorsinkhof@gmail.com>
2025-09-01 09:33:28 +0200

    added pendulum

--- include/physics.h
+++ include/physics.h
@@ -10,4 +10,6 @@ typedef void (*Model)(float state[], float ddt_state[]);   // the API shape
 /* Models*/
 void simple_model(float state[], float ddt_state[]);
 
+void pendulum(float state[], float ddt_state[]);
+
 #endif
--- src/physics.c
+++ src/physics.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 #include "physics.h"
 
 void simple_model(float state[], float ddt_state[]){
     ddt_state[0] = -2*state[0] + state[1]*state[0];
     ddt_state[1] = -1*state[1];
+}
+
+void pendulum(float state[], float ddt_state[]){
+    float g = 9.81;
+    float l = 1;
+    ddt_state[0] = state[1];
+    ddt_state[1] = -(g/l)*sin(state[0]);
 }
