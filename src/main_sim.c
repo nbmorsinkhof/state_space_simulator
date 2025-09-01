251347f2e609dba6b2ac5cff84bda20816e7db5a
Niek <niekmorsinkhof@gmail.com>
2025-09-01 09:33:44 +0200

    run sim in thread

--- src/main_sim.c
+++ src/main_sim.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
 
 #include "simulator.h"
 #include "physics.h"
 #include "integrator.h"
 
+
+
+typedef struct {
+    Simulator* simulator;
+    float *state_initial;
+    float sim_time;
+    float dt;
+} Simargs;
+
+void *runThread(void* arg){
+    Simargs* args = (Simargs*) arg;
+    
+    run_simulation(
+        args->simulator,
+        (args->state_initial),
+        args->sim_time,
+        args->dt
+    );
+    return NULL;
+}
+
 int main(void){
-    int dim = 2;
+
+    Simargs* sim_args = malloc(sizeof(Simargs));
     /* init simulator*/
-    Simulator* simulator = init_simulation(
-        simple_model,
+    int dim = 2;
+    sim_args->simulator = init_simulation(
+        pendulum,
         euler,
         dim
     );
-    float state_initial[] = {5.0, 2.0};
-    float sim_time = 10.0;
-    float dt = 0.1;
-    run_simulation(
-        simulator,
-        state_initial,
-        sim_time,
-        dt
-    );
+    float state_initial[] = {-0.1*3.14, 0};
+    sim_args->state_initial = state_initial;
+    sim_args->sim_time = 10.0;
+    sim_args->dt = 0.01;
 
+
+    pthread_t sim_thread;
+    pthread_create(&sim_thread, NULL, runThread, sim_args);
+    pthread_join(sim_thread, NULL);
+
+    free(sim_args);
     return 0;
 }
